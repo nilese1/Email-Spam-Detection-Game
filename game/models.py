72c21667306da9026cf48b7365c9024fac301cfe
Ethan Niles <notethansemail123456789@gmail.com>
2025-04-06 15:36:15 -0400

    Added models and wrote base template

--- game/models.py
+++ game/models.py
@@ -1,3 +1,39 @@
+from typing import final
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# The choices for what different difficulties there can be
+# Other choices will throw an ValidationError if other values attempted to be saved
+DIFFICULTY_CHOICES = ["Easy", "Medium", "Hard"]
+
+
+@final
+class Game(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    difficulty_level = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
+    score = models.BigIntegerField(null=True)
+    start_time = models.DateTimeField(auto_now=True)
+    end_time = models.DateTimeField(null=True)
+
+
+@final
+class Email(models.Model):
+    subject = models.CharField(max_length=100)
+    sender = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    attachment_url = models.URLField(null=True)
+    difficulty_level = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
+    spam_indicator = models.BooleanField()
+
+
+@final
+class GameEmail(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+
+
+@final
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    submissionDate = models.DateTimeField(auto_now=True)
