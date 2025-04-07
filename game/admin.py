3a2391c65853f90a502074e6d3d6896f4d21297c
Ethan Niles <notethansemail123456789@gmail.com>
2025-04-07 16:11:46 -0400

    added admin panel functionality for emails

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from typing import final
+from .models import Email
 
 # Register your models here.
+
+@final
+class EmailAdmin(admin.ModelAdmin):
+    fieldsets = []
+
+    list_display = ["subject", "difficulty_level", "spam_indicator"]
+
+
+admin.site.register(Email, EmailAdmin)
--- game/models.py
+++ game/models.py
@@ -4,14 +4,15 @@ from django.contrib.auth.models import User
 
 # The choices for what different difficulties there can be
 # Other choices will throw an ValidationError if other values attempted to be saved
-DIFFICULTY_CHOICES = ["Easy", "Medium", "Hard"]
+# in the format (actual name, human readable name)
+DIFFICULTY_CHOICES = [("e", "Easy"), ("m", "Medium"), ("h", "Hard")]
 
 
 @final
 class Game(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     difficulty_level = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
-    score = models.BigIntegerField(null=True)
+    score = models.BigIntegerField()
     start_time = models.DateTimeField(auto_now=True)
     end_time = models.DateTimeField(null=True)
 
